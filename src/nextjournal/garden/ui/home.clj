cc5e4f1ad272327f2e4538bdd1b29325b85f6cf7
Martin Kavalar <martin@nextjournal.com>
2022-09-20 10:40:59 -0400

    Bump Clerk and upgrade viewers

--- src/nextjournal/garden/ui/home.clj
+++ src/nextjournal/garden/ui/home.clj
@@ -1,7 +1,6 @@
 ;; # 🌳 Clerk Garden
-
-^{:nextjournal.clerk/visibility :hide}
 (ns nextjournal.garden.ui.home
+  {:nextjournal.clerk/visibility {:code :hide :result :hide}}
   (:require [nextjournal.clerk :as clerk]))
 
 ;; Welcome, friend. **clerk.garden** is a simple publishing space for Clerk notebooks.
@@ -10,25 +9,24 @@
 ;; ## 1️⃣ Add a `:nextjournal/clerk` alias to your `deps.edn` file.
 ;; It must contain, at least, the following values:
 
-(clerk/html
-  {::clerk/width :wide}
-  [:div.viewer.viewer-code.not-prose
-   (clerk/code "{:deps ,,,
-:aliases
-{:nextjournal/clerk
- {:exec-fn nextjournal.clerk/build-static-app!
-  :exec-args {:paths [\"src/nextjournal/garden/ui/builder.clj\"
-                      ;; TODO: add all notebooks you want to have built here
-                      ]}}})")])
+(defn code-listing [code]
+  (clerk/html {::clerk/width :wide} [:div.viewer.viewer-code.not-prose (clerk/code code)]))
+
+{:nextjournal.clerk/visibility {:code :hide :result :show}}
+
+
+
+(code-listing
+ "{:deps ,,,
+ :aliases {:nextjournal/clerk {:exec-fn nextjournal.clerk/build-static-app!
+                               :exec-args {:paths [\"src/nextjournal/garden/ui/builder.clj\" ]}}}}
+                               ;; TODO: add all notebooks you want to have built ☝️")
 
 ;; ## 2️⃣  Try it out locally to make sure it works
 ;; Entering the following command into your Terminal should build your notebooks and open an
 ;; index page in your browser.
 
-(clerk/html
-  {::clerk/width :wide}
-  [:div.viewer.viewer-code.not-prose
-   (clerk/code "clj -X:nextjournal/clerk")])
+(code-listing "clj -X:nextjournal/clerk")
 
 ;; ## ✅  Push it to GitHub and done!
 ;; **clerk.garden** mirrors your GitHub URLs, so:
